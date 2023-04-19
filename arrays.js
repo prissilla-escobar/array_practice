2d37cdebd0be9b7a1b51aa56e6b712eb86e87f80
Prissilla Escobar <prissilla988@gmail.com>
2023-04-19 16:57:44 -0600

    Add comment of my understanding of what an index position is

--- arrays.js
+++ arrays.js
@@ -1,4 +1,7 @@
+
+
 var niblings = ["Kailan", "Luis", "Caleb", "Adalyn", "Avery"]
+// nibling is a niece or nephew! haha I read it's a new word to combine the two
 var niblingsAges = [15, 14, 11, 5, 2]
 var activities = ["sport player", "artist", "risk taker", "couch potato"]
 
@@ -15,3 +18,8 @@ console.log(niblingsAges)
 line 17 will print the output */
 activities.push("runner")
 console.log(activities)
+
+/* Index positions is the number an element is read in an array from left to right.
+When counting in an array, you will always start with the number 0 and then go up 
+from there from left to right. Below is the index positions for the variable 'niblings'
+Kailan is 0, Luis is 1, Caleb is 2, Adalyn is 3 and Avery is 4.
