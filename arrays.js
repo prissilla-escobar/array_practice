2854cec46377a2b715bf701681d6600c7888e304
Prissilla Escobar <prissilla988@gmail.com>
2023-04-19 16:48:37 -0600

    Add one array method for each variable and comment an explanation of the impact

--- arrays.js
+++ arrays.js
@@ -2,3 +2,16 @@ var niblings = ["Kailan", "Luis", "Caleb", "Adalyn", "Avery"]
 var niblingsAges = [15, 14, 11, 5, 2]
 var activities = ["sport player", "artist", "risk taker", "couch potato"]
 
+// this method will remove the last element in the array. line 7 will print the output.
+niblings.pop()
+console.log(niblings)
+
+// This method will add the element in the parenthesis to the beginning of the array.
+// line 11 will print the output
+niblingsAges.unshift(67)
+console.log(niblingsAges)
+
+// This method will add the element in the parenthesis to the end of the array.
+// line 17 will print the output
+activities.push("runner")
+console.log(activities)
